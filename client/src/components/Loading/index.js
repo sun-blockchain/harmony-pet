5091a956e571b05c7e5fee9fa7120b0d4f069497
tranchien <chientm@apples-MacBook-Pro.local>
2020-08-18 15:00:40 +0700

    add signin signout

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Button } from 'reactstrap';
+import * as actions from 'actions';
 const Loading = () => {
+  const dispatch = useDispatch();
   return (
     <div className='container-custom'>
       <img alt='' src={require('assets/img/lg.blue-longcat-spinner.gif')} className='gif-load' />
+      <br />
+      <Button color='success' onClick={() => dispatch(actions.signInWallet())}>
+        Sign in
+      </Button>
     </div>
   );
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import tomoReducer from './tomoReducer';
+import reducer from './harmonyReducer';
 
 const rootReducer = combineReducers({
-  tomo: tomoReducer
+  harmony: reducer
 });
 
 export default rootReducer;

da722939963ffdd755089ba70fd17057667c9494
trinhtan <lop12a1trinhvantan@gmail.com>
2020-08-19 14:51:05 +0700

    convertAddress

--- client/src/blockchain/index.js
+++ client/src/blockchain/index.js
@@ -25,21 +25,9 @@ const options = {
 
 const instance = petFactory.methods;
 
-// export const getList = async () => {
-//   let total = await instance.totalSupply().call(options);
-
-//   const cards = [];
-
-//   for (let i = 0; i < total; i++) {
-//     let res = await instance.getPlayer(i).call(options);
-//     cards.push(res);
-//   }
-
-//   return cards;
-// };
-exports.getAllPetAddressOf = async function (address) {
+exports.getAllPetAddressOf = async function (oneAddress) {
   try {
-    let a = await hmy.crypto.getAddress(address).bech32;
+    let address = await hmy.crypto.getAddress(oneAddress).checksum;
     console.log(a);
     let data = await instance.getAllPetAddressOf(address).call(options);
     let response = {
@@ -58,59 +46,4 @@ exports.getAllPetAddressOf = async function (address) {
   }
 };
 
-// export const getPlayerById = async id => {
-//   const res = await instance.getPlayer(id).call(options);
-
-//   return res;
-// };
-
-// export const getTotalPlayers = async () => {
-//   const res = await instance.totalSupply().call(options);
-
-//   return res;
-// };
-
-// export const buyPlayerById = (params: {
-//   id: string,
-//   price: string,
-//   signer: string,
-// }): Promise<{ status: string, transaction: { id: string } }> => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       soccerPlayers.wallet.defaultSigner = params.signer;
-
-//       soccerPlayers.wallet.signTransaction = async tx => {
-//         try {
-//           tx.from = params.signer;
-//           // @ts-ignore
-//           const signTx = await window.harmony.signTransaction(tx);
-
-//           // const [sentTx, txHash] = await signTx.sendTransaction();
-
-//           // await sentTx.confirm(txHash);
-
-//           // resolve(txHash);
-//           return signTx;
-//         } catch (e) {
-//           console.error(e);
-//           reject(e);
-//         }
-
-//         return null;
-//       };
-
-//       const res = await soccerPlayers.methods
-//         .purchase(params.id)
-//         .send({ ...options, value: params.price });
-
-//       resolve(res);
-//     } catch (e) {
-//       console.error(e);
-
-//       reject(e);
-//     }
-//   });
-// };
-
-// export const getBech32Address = address => hmy.crypto.getAddress(address).bech32;
 this.getAllPetAddressOf('one12j4ycvnta3l68ep28lpe73n20wx470yfzq9uf3');
